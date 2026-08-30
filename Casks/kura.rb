2d200823ac406989c21dd73207f7a882d054b168
jeu <dinggi5@proton.me>
2026-08-31 06:52:47 +0900

    kura 0.3.1

--- Casks/kura.rb
+++ Casks/kura.rb
@@ -1,6 +1,6 @@
 cask "kura" do
-  version "0.3.0"
-  sha256 "9d972463ffbb827cea3fcfa3658bac801121e1c59e0bfc0316046dfd5d6c1a9c"
+  version "0.3.1"
+  sha256 "2e0f2e7e912111b2e68092b8bbe763ba638ed758ca8dc00e5f83440138bec878"
 
   url "https://github.com/dinggi5/kura/releases/download/v#{version}/Kura_#{version}_aarch64.dmg",
       verified: "github.com/dinggi5/kura/"
